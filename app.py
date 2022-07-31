e369605bce8dd981794f0ddbff6133222b78dcba
Maria Luque <s1649104@ed.ac.uk>
2022-07-31 19:45:53 +0100

    idk

--- app.py
+++ app.py
@@ -1,50 +1,263 @@
-from flask import Flask, request, Response
-from flask_restful import Resource, Api
-from qa_text import get_document_store, get_retriever, get_reader, create_pipeline, ask_question
+import os
+import sys
+import logging
+from pathlib import Path
+from json import JSONDecodeError
+
+import pandas as pd
+import streamlit as st
+from annotated_text import annotation
+from markdown import markdown
+
+from utils import haystack_is_ready, query, send_feedback, haystack_version, get_backlink
 
 
-app = Flask(__name__)
-api = Api(app)
+# Adjust to a question that you would like users to see in the search bar when they load the UI:
+DEFAULT_QUESTION_AT_STARTUP = os.getenv("DEFAULT_QUESTION_AT_STARTUP", "What's the capital of France?")
+DEFAULT_ANSWER_AT_STARTUP = os.getenv("DEFAULT_ANSWER_AT_STARTUP", "Paris")
 
-# initialise document store
-document_store = get_document_store()
+# Sliders
+DEFAULT_DOCS_FROM_RETRIEVER = int(os.getenv("DEFAULT_DOCS_FROM_RETRIEVER", "3"))
+DEFAULT_NUMBER_OF_ANSWERS = int(os.getenv("DEFAULT_NUMBER_OF_ANSWERS", "3"))
 
-# get retrievers
-retriever1, retriever2 = get_retriever(document_store)
-
-# get reader
-reader = get_reader()
-
-# combine them into a pipeline
-pipeline = create_pipeline(reader, retriever2)
-
-@app.route('/initialized', methods=['GET'])
-def respond():
-    return Response("yes it works", status=201)
-
-@app.route('/query', methods=['POST'])
-def query():
-
-    print(request.args)
-    # req: {'query': "What's the capital of France?", 'params': {'filters': {}, 'Retriever': {'top_k': 3}, 'Reader': {'top_k': 3}}}
-    
-    # ask_question(pipeline, question)
-
-    return Response("WOOP WOOP", status=201)
-    
-
-class Ping(Resource):
-    def get(self):
-        return 'pong', 200 
-
-@app.route('/')
-def index():
-    # A welcome message to test our server
-    return "<h2>Maria Luque Anguita - Space diss</h2>"
-
-api.add_resource(Ping, '/ping')
+# Labels for the evaluation
+EVAL_LABELS = os.getenv("EVAL_FILE", str(Path(__file__).parent / "eval_labels_example.csv"))
 
 
-if __name__ == '__main__':
-    app.run(host="localhost", port=8000, debug=True) 
+def set_state_if_absent(key, value):
+    if key not in st.session_state:
+        st.session_state[key] = value
 
+
+def main():
+
+    st.set_page_config(page_title="Space QA", page_icon="https://raw.githubusercontent.com/marialuquea/mariasounds/master/images/favicon.ico")
+
+    # Persistent state
+    set_state_if_absent("question", DEFAULT_QUESTION_AT_STARTUP)
+    set_state_if_absent("answer", DEFAULT_ANSWER_AT_STARTUP)
+    set_state_if_absent("results", None)
+    set_state_if_absent("raw_json", None)
+    set_state_if_absent("random_question_requested", False)
+
+    # Small callback to reset the interface in case the text of the question changes
+    def reset_results(*args):
+        st.session_state.answer = None
+        st.session_state.results = None
+        st.session_state.raw_json = None
+
+    # Title and introduction
+    st.write("# Space Q&A")
+    st.markdown(
+        """
+Ask questions about space debris, management and safety :)
+
+*Note: do not use keywords, but full-fledged questions.*
+""",
+        unsafe_allow_html=True,
+    )
+
+    # Sidebar
+    st.sidebar.header("Options")
+    top_k_reader = st.sidebar.slider(
+        "Max. number of answers",
+        min_value=1,
+        max_value=10,
+        value=DEFAULT_NUMBER_OF_ANSWERS,
+        step=1,
+        on_change=reset_results,
+    )
+    top_k_retriever = st.sidebar.slider(
+        "Max. number of documents from retriever",
+        min_value=1,
+        max_value=10,
+        value=DEFAULT_DOCS_FROM_RETRIEVER,
+        step=1,
+        on_change=reset_results,
+    )
+    eval_mode = st.sidebar.checkbox("Evaluation mode")
+    debug = st.sidebar.checkbox("Show debug info")
+
+    hs_version = ""
+    try:
+        hs_version = f" <small>(v{haystack_version()})</small>"
+    except Exception:
+        pass
+
+    st.sidebar.markdown(
+        f"""
+    <style>
+        a {{
+            text-decoration: none;
+        }}
+        .haystack-footer {{
+            text-align: center;
+        }}
+        .haystack-footer h4 {{
+            margin: 0.1rem;
+            padding:0;
+        }}
+        footer {{
+            opacity: 0;
+        }}
+    </style>
+    <div class="haystack-footer">
+        <hr />
+
+        Maria Luque Anguita
+        s1649104
+    </div>
+    """,
+        unsafe_allow_html=True,
+    )
+
+    # Load csv into pandas dataframe
+    try:
+        df = pd.read_csv(EVAL_LABELS, sep=";")
+    except Exception:
+        st.error(
+            f"The eval file was not found. Please check the demo's [README](https://github.com/deepset-ai/haystack/tree/master/ui/README.md) for more information."
+        )
+        sys.exit(
+            f"The eval file was not found under `{EVAL_LABELS}`. Please check the README (https://github.com/deepset-ai/haystack/tree/master/ui/README.md) for more information."
+        )
+
+    # Search bar
+    question = st.text_input(
+        "", 
+        value=st.session_state.question, 
+        max_chars=100, 
+        on_change=reset_results
+    )
+    col1, col2 = st.columns(2)
+    col1.markdown("<style>.stButton button {width:100%;}</style>", unsafe_allow_html=True)
+    col2.markdown("<style>.stButton button {width:100%;}</style>", unsafe_allow_html=True)
+
+    # Run button
+    run_pressed = col1.button("Run")
+
+    # Get next random question from the CSV
+    if col2.button("Random question"):
+        reset_results()
+        new_row = df.sample(1)
+        while (
+            new_row["Question Text"].values[0] == st.session_state.question
+        ):  # Avoid picking the same question twice (the change is not visible on the UI)
+            new_row = df.sample(1)
+        st.session_state.question = new_row["Question Text"].values[0]
+        st.session_state.answer = new_row["Answer"].values[0]
+        st.session_state.random_question_requested = True
+        # Re-runs the script setting the random question as the textbox value
+        # Unfortunately necessary as the Random Question button is _below_ the textbox
+        raise st.scriptrunner.script_runner.RerunException(st.scriptrunner.script_requests.RerunData(None))
+    st.session_state.random_question_requested = False
+
+    run_query = (
+        run_pressed or question != st.session_state.question
+    ) and not st.session_state.random_question_requested
+
+    # Check the connection
+    with st.spinner("⌛️ &nbsp;&nbsp; Loading the Space QA..."):
+        if not haystack_is_ready():
+            st.error("🚫 &nbsp;&nbsp; Connection Error. Speak to Maria to fix it :)")
+            run_query = False
+            reset_results()
+
+    # Get results for query
+    if run_query and question:
+        reset_results()
+        st.session_state.question = question
+
+        with st.spinner("🧠 &nbsp;&nbsp; Performing neural search on documents... \n "):
+            try:
+                st.session_state.results, st.session_state.raw_json = query(
+                    question, 
+                    top_k_reader    = top_k_reader, 
+                    top_k_retriever = top_k_retriever
+                )
+            except JSONDecodeError as je:
+                st.error("👓 &nbsp;&nbsp; An error occurred reading the results. Check document store connection.")
+                return
+            except Exception as e:
+                logging.exception(e)
+                if "The server is busy processing requests" in str(e) or "503" in str(e):
+                    st.error("🧑‍🌾 &nbsp;&nbsp; All workers are busy! Try again later.")
+                else:
+                    st.error("🐞 &nbsp;&nbsp; An error occurred during the request.")
+                return
+
+    if st.session_state.results:
+
+        # Show the gold answer if we use a question of the given set
+        if eval_mode and st.session_state.answer:
+            st.write("## Correct answer:")
+            st.write(st.session_state.answer)
+
+        st.write("## Results:")
+
+        for count, result in enumerate(st.session_state.results):
+            if result["answer"]:
+                answer, context = result["answer"], result["context"]
+                start_idx = context.find(answer)
+                end_idx = start_idx + len(answer)
+                # Hack due to this bug: https://github.com/streamlit/streamlit/issues/3190
+                st.write(
+                    markdown(context[:start_idx] + str(annotation(answer, "ANSWER", "#8ef")) + context[end_idx:]),
+                    unsafe_allow_html=True,
+                )
+                source = ""
+                url, title = get_backlink(result)
+                if url and title:
+                    source = f"[{result['document']['meta']['title']}]({result['document']['meta']['url']})"
+                else:
+                    source = f"{result['source']}"
+                st.markdown(f"**Relevance:** {result['relevance']} -  **Source:** {source}")
+
+            else:
+                st.info(
+                    "🤔 &nbsp;&nbsp; Haystack is unsure whether any of the documents contain an answer to your question. Try to reformulate it!"
+                )
+                st.write("**Relevance:** ", result["relevance"])
+
+            if eval_mode and result["answer"]:
+                # Define columns for buttons
+                is_correct_answer = None
+                is_correct_document = None
+
+                button_col1, button_col2, button_col3, _ = st.columns([1, 1, 1, 6])
+                if button_col1.button("👍", key=f"{result['context']}{count}1", help="Correct answer"):
+                    is_correct_answer = True
+                    is_correct_document = True
+
+                if button_col2.button("👎", key=f"{result['context']}{count}2", help="Wrong answer and wrong passage"):
+                    is_correct_answer = False
+                    is_correct_document = False
+
+                if button_col3.button(
+                    "👎👍", key=f"{result['context']}{count}3", help="Wrong answer, but correct passage"
+                ):
+                    is_correct_answer = False
+                    is_correct_document = True
+
+                if is_correct_answer is not None and is_correct_document is not None:
+                    try:
+                        send_feedback(
+                            query=question,
+                            answer_obj=result["_raw"],
+                            is_correct_answer=is_correct_answer,
+                            is_correct_document=is_correct_document,
+                            document=result["document"],
+                        )
+                        st.success("✨ &nbsp;&nbsp; Thanks for your feedback! &nbsp;&nbsp; ✨")
+                    except Exception as e:
+                        logging.exception(e)
+                        st.error("🐞 &nbsp;&nbsp; An error occurred while submitting your feedback!")
+
+            st.write("___")
+
+        if debug:
+            st.subheader("REST API JSON response")
+            st.write(st.session_state.raw_json)
+
+
+main()
